8b56efed6ba4df659f51337beadaea2205d28f94
dmytro.relkin <dmytro.relkin@globallogic.com>
2019-11-18 23:28:02 +0200

    Init the FireBase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: "AIzaSyA45EPy0ZucETvoNMCp9dfxtb5yU44Kzio",
+  authDomain: "react-web-app-cource-a1413.firebaseapp.com",
+  databaseURL: "https://react-web-app-cource-a1413.firebaseio.com",
+  projectId: "react-web-app-cource-a1413",
+  storageBucket: "react-web-app-cource-a1413.appspot.com",
+  messagingSenderId: "642948747872",
+  appId: "1:642948747872:web:b71002891b7745c221a905"
+};
+
+export default config;
