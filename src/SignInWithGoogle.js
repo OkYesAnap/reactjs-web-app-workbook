3788e9e76c9da5aef9702a3ed2340fb52038a453
dmytro.relkin <dmytro.relkin@globallogic.com>
2019-11-25 23:10:59 +0200

    Has been added the example of connect the FireBase

---
+++ src/SignInWithGoogle.js
@@ -0,0 +1,41 @@
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
+import React from 'react';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+class SignInWithGoogle extends React.Component {
+  render() {
+    const {
+      user,
+      signOut,
+      signInWithGoogle,
+    } = this.props;
+
+    return (
+      <div className="login-wrapper">
+        {
+          user
+            ? <p>Hello, {user.displayName}</p>
+            : <p>Please sign in.</p>
+        }
+        {
+          user
+            ? <button onClick={signOut}>Sign out</button>
+            : <button onClick={signInWithGoogle}>Sign in with Google</button>
+        }
+      </div>
+    );
+  }
+}
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(SignInWithGoogle);
