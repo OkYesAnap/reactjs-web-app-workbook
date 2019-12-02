40c96b157a87a7976b4626358ee289e646f802bd
Andrii Panaseiko <Andrii.Panaseiko@globallogic.com>
2019-12-02 18:06:48 +0200

    test

--- src/SignInWithGoogle.js
+++ src/SignInWithGoogle.js
@@ -1,41 +1,48 @@
 import withFirebaseAuth from 'react-with-firebase-auth'
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
+import firebase from 'firebase';
 import firebaseConfig from './firebaseConfig';
 import React from 'react';
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const firebaseAppAuth = firebaseApp.auth();
+
 const providers = {
-  googleProvider: new firebase.auth.GoogleAuthProvider(),
+    googleProvider: new firebase.auth.GoogleAuthProvider(),
 };
 
-class SignInWithGoogle extends React.Component {
-  render() {
-    const {
-      user,
-      signOut,
-      signInWithGoogle,
-    } = this.props;
 
-    return (
-      <div className="login-wrapper">
-        {
-          user
-            ? <p>Hello, {user.displayName}</p>
-            : <p>Please sign in.</p>
-        }
-        {
-          user
-            ? <button onClick={signOut}>Sign out</button>
-            : <button onClick={signInWithGoogle}>Sign in with Google</button>
-        }
-      </div>
-    );
-  }
+class SignInWithGoogle extends React.Component {
+    write(){
+        return firebase.database().ref().once('value', (spapshot)=>console.error(spapshot.val()))         
+    }
+
+    render() {
+
+
+        const {
+            user,
+            signOut,
+            signInWithGoogle,
+        } = this.props;
+        return (
+            <div className="login-wrapper">
+                {
+                    user
+                        ? <p>Hello, {user.displayName}</p>
+                        : <p>Please sign in.</p>
+                }
+                {
+                    user
+                        ? <button onClick={signOut}>Sign out</button>
+                        : <button onClick={signInWithGoogle}>Sign in with Google</button>
+                }
+              <div><button onClick={this.write}>Show characters</button></div>
+            </div>
+        );
+    }
 }
 
 export default withFirebaseAuth({
-  providers,
-  firebaseAppAuth,
+    providers,
+    firebaseAppAuth,
 })(SignInWithGoogle);
